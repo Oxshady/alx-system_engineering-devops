2b4e1f20fb526018b9ab67feeb698dfbf3d9567e
Oxshady <shadymahmod55@gmail.com>
2024-06-28 20:48:04 +0300

    package resource

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # this manifest install python package using pip
-exec {'flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  =>  '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0"',
-  path    => ['/usr/bin', 'bin']
+package {'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
