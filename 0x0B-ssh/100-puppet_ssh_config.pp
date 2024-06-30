f0c4102444f670820416d01cc015c7870bbe25c7
Oxshady <shadymahmod55@gmail.com>
2024-06-30 09:51:21 +0300

    configure ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,6 @@
 # configure ssh
 # disable password authentication
 # use school priv key
-service { 'sshd':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  hasstatus  => true,
-  provider   => 'systemd'
-}
 
 file_line {'ssh_key':
     ensure  => 'present',
@@ -15,9 +8,7 @@ file_line {'ssh_key':
     path    => '/etc/ssh/sshd_config',
     match   => "^#*\s*IdentityFile\s+~/.ssh/id_rsa$"
     replace => true,
-    notify  => Exec['sshd_restart'],
 }
-
 
 file_line {'ssh_pass':
     ensure  => 'present',
@@ -25,11 +16,4 @@ file_line {'ssh_pass':
     path    => '/etc/ssh/sshd_config',
     match   => "^#*\s*PasswordAuthentication\s+yes$"
     replace => true,
-    notify  => Exec['sshd_restart'],
-}
-
-exec { 'sshd_restart':
-  path        => '/usr/bin:/bin',
-  command     => '/bin/systemctl restart sshd',
-  refreshonly => true
 }
