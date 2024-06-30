af6437c3cd1e3c7e1b35e64ef254fe91e3ff8f2b
Oxshady <shadymahmod55@gmail.com>
2024-06-30 10:12:45 +0300

    ^_^

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,19 +4,19 @@
 include stdlib
 
 file_line {'ssh_key':
-    line               => '    IdentityFile ~/.ssh/school',
-    path               => '/etc/ssh/sshd_config',
-    match              => "^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$"
-    replace            => true,
-    append_on_no_match => true
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
 
 file_line {'ssh_pass':
-    line               => '    PasswordAuthentication no',
-    path               => '/etc/ssh/sshd_config',
-    match              => "^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$"
-    replace            => true,
-    append_on_no_match => true
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
 
