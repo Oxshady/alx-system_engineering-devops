5520ffb2106da5b09f62021cda4dd70dfc4859fb
Oxshady <shadymahmod55@gmail.com>
2024-06-30 10:01:53 +0300

    configure ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,20 @@
 # configure ssh
 # disable password authentication
 # use school priv key
+include stdlib
+
 
 file_line {'ssh_key':
-    ensure  => 'present',
-    line    => '\tIdentityFile ~/.ssh/school',
-    path    => '/etc/ssh/ssh_config',
-    match   => "^#*\s*IdentityFile\s+~/.ssh/id_rsa$"
-    replace => true,
+    ensure => 'present',
+    line   => '\tIdentityFile ~/.ssh/school',
+    path   => '/etc/ssh/ssh_config',
+    match  => "^#*\s*IdentityFile\s+~/.ssh/id_rsa$"
 }
+
 
 file_line {'ssh_pass':
-    ensure  => 'present',
-    line    => '\tPasswordAuthentication no',
-    path    => '/etc/ssh/ssh_config',
-    match   => "^#*\s*PasswordAuthentication\s+yes$"
-    replace => true,
+    ensure => 'present',
+    line   => '\tPasswordAuthentication no',
+    path   => '/etc/ssh/ssh_config',
+    match  => "^#*\s*PasswordAuthentication\s+yes$"
 }
-
