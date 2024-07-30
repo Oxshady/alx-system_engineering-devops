a0f92137e8ad18b2385ae9c04bacdc4ffd0180f1
Oxshady <shadymahmod55@gmail.com>
2024-07-30 03:27:35 +0300

    done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
-import requests
 import json
+import requests
 from sys import argv
 
 if __name__ == "__main__":
