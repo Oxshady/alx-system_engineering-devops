0a763c08d31bd12bd42c9904708237236fa86924
Oxshady <shadymahmod55@gmail.com>
2024-07-30 03:05:10 +0300

    save as json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    todo = requests.get(url=url).json()
+    user = requests.get(url=f"https://jsonplaceholder.typicode.com/users/{id}")
+    user = user.json().get('username')
+    data = {}
+    datalist = []
+    for toodo in todo:
+        if str(toodo.get("userId")) == id:
+            data["task"] = toodo.get("title")
+            data["completed"] = toodo.get("completed")
+            data["username"] = user
+            datalist.append(data)
+    final = {f"{id}": datalist}
+    with open(f"{id}.json", 'a') as jsonf:
+        json.dump(final, jsonf)
