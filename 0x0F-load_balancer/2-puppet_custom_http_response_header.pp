bfd6ec41d8bc1ecae45044f95d56921f3fcb2c2a
Oxshady <shadymahmod55@gmail.com>
2024-08-02 19:20:29 +0300

    configure using cms pupped

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,32 @@
+# automating the configuration of nginx
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+	notify  => Service['nginx'],
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+	notify  => Service['nginx'],
+}
+
+exec {'custom_header':
+	command => 'sed -i "26i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default"'
+	provider => 'shell'
+	notify  => Service['nginx'],
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
