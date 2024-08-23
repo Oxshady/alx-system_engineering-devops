aa24ec987ca974c2cd299d0623e84e8df706c621
Oxshady <shadymahmod55@gmail.com>
2024-08-23 08:26:34 +0300

    upda

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,5 @@
 # new admin
-
-$file_path = '/etc/default/nginx'
-$line = 'ULIMIT="-n 4096"'
-
-exec { 'modify':
-        provider => 'shell',
-        command  => "sed -i 's/^ULIMIT=.*/${line}/' ${file_path}",
-        unless   => "grep -q '^${line}\$' ${file_path}",
-}
-
-exec { 'restart':
-        provider => 'shell',
-        command  => 'sudo service nginx restart',
+exec { 'Fix hard limit':
+    provider => 'shell',
+    command  => 'sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf'
 }
